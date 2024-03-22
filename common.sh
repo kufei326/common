a6e86434c6f7c27a5c4b77644686a3a1fd9f62c8
kufei326 <73808426+kufei326@users.noreply.github.com>
2024-03-22 15:15:22 +0800

    Update common.sh

--- common.sh
+++ common.sh
@@ -71,6 +71,8 @@ function parse_settings() {
 	fi
 	if [[ $NEW_KERNEL_PATCHVER =~ (default|DEFAULT|Default) || -z $NEW_KERNEL_PATCHVER ]]; then
         NEW_KERNEL_PATCHVER="0"  # 使用默认值 0
+	echo NEW_KERNEL_PATCHVER="$NEW_KERNEL_PATCHVER" >> ${GITHUB_ENV}
+        __info_msg "NEW_KERNEL_PATCHVER 变量的值为：$NEW_KERNEL_PATCHVER"
         fi
 	case "$SOURCE_ABBR" in
 	lede|Lede|LEDE)
